1f6b6d0d2c847ff151ddaa04a20e831c89259137
kupiakos <kupiakos@gmail.com>
2016-12-07 00:14:00 -0700

    Add some extra asserts in PQueue

--- pqueue.c
+++ pqueue.c
@@ -23,6 +23,7 @@ PQueue initQ() {
 }
 
 int enQ(PQueue q, TID tid, Priority p) {
+    assert(q);
     PQueue entry = (q + ++q[0].size);
     entry->tid = tid;
     entry->priority = p;
@@ -30,12 +31,14 @@ int enQ(PQueue q, TID tid, Priority p) {
 }
 
 static void delElement(PQueue q, int i) {
+    assert(q);
     assert(q[0].size > 0);
     memmove(q + i, q + i + 1, (q[0].size - i) * sizeof(*q));
     --q[0].size;
 }
 
 static int tidElement(PQueue q, TID tid) {
+    assert(q);
     for (int i = 1; i <= q[0].size; ++i) {
         if (q[i].tid == tid) {
             return i;
@@ -45,6 +48,7 @@ static int tidElement(PQueue q, TID tid) {
 }
 
 static int highestElement(PQueue q) {
+    assert(q);
     int highest = -1, highestIndex = -1;
 
     for (int i = 1; i <= q[0].size; ++i) {
@@ -57,6 +61,7 @@ static int highestElement(PQueue q) {
 }
 
 int deQ(PQueue q, TID tid) {
+    assert(q);
     int idx;
     if (tid == -1) {
         idx = highestElement(q);
@@ -72,11 +77,13 @@ int deQ(PQueue q, TID tid) {
 }
 
 void delQ(PQueue *q) {
+    assert(q && *q);
     free(*q);
     *q = NULL;
 }
 
 size_t lenQ(PQueue q) {
+    assert(q);
     return q[0].size;
 }
 
@@ -87,6 +94,7 @@ int compPriority(const void *i, const void *j) {
 }
 
 size_t listQ(PQueue q, TID *tids) {
+    assert(q);
     qsort(q + 1, q[0].size, sizeof(*q), &compPriority);
     for (int i = 1; i <= q[0].size; ++i) {
         tids[i - 1] = q[i].tid;
